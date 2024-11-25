ef341cd41c589c0b3ab8656c6cf87b1819f1c047
RoxanaSultan <roxana.sultan@student.unitbv.ro>
2024-11-25 15:47:55 +0200

    Added files to Register

--- app/src/main/java/com/example/memoraid/RegisterAccountInformationFragment.kt
+++ app/src/main/java/com/example/memoraid/RegisterAccountInformationFragment.kt
@@ -10,7 +10,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 
 class RegisterAccountInformationFragment : Fragment() {
-//    private lateinit var navController: NavController
+    private lateinit var navController: NavController
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,15 +18,15 @@ class RegisterAccountInformationFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_register_account_information, container, false)
     }
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//        navController = NavHostFragment.findNavController(this)
-//
-//        val firstContinueButton: Button = view.findViewById(R.id.first_register_continue_button)
-//
-//        firstContinueButton.setOnClickListener {
-//            findNavController().navigate(R.id.fragment_register_optional_account_info)
-//        }
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = NavHostFragment.findNavController(this)
+
+        val firstContinueButton: Button = view.findViewById(R.id.first_register_continue_button)
+
+        firstContinueButton.setOnClickListener {
+            findNavController().navigate(R.id.fragment_register_optional_account_info)
+        }
+    }
 }
